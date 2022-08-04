df7c0608eb9b6473230690293b1d1d36c09b3807
Ernesto Murrugarra <ernestomurrudev@gmail.com>
2022-08-03 23:35:20 -0400

    Finished whatsapp me test

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { computed, ref } from 'vue'
+
+const phoneNumber = ref('')
+const disabledBtn = computed(() => { return phoneNumber.value === '' })
+
+function startChat() {
+  window.open(`https://wa.me/${phoneNumber.value}`, '_blank')
+}
+</script>
+
+<template>
+  <input
+    type="text"
+    name="wa-me-number"
+    id="waMeNumber"
+    placeholder="Number without +, 0 or country code"
+    class="waMeNumber"
+    v-model="phoneNumber"
+  >
+  <br><br>
+  <button
+    type="button"
+    @click="startChat"
+    :class="{disabledButton: disabledBtn}"
+    :disabled="disabledBtn"
+  >Start chat</button>
+</template>
+
+<style scoped>
+.waMeNumber {
+  width: 300px;
+  padding: 10px;
+}
+
+.disabledButton {
+  background-color: grey;
+  color: white;
+}
+</style>
