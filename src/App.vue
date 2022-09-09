a5e46fc7d642ca973ec19a6880b8ddff26e7c9c6
Ernesto Murrugarra <ernestomurrudev@gmail.com>
2022-09-09 00:34:21 -0400

    Added last changes. Now it looks better

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,120 @@ import { computed, ref } from 'vue'
 const phoneNumber = ref('')
 const disabledBtn = computed(() => { return phoneNumber.value === '' })
 
+function NumbersOnly($evt) {
+  $evt = ($evt) ? $evt : window.event;
+  var charCode = ($evt.which) ? $evt.which : $evt.keyCode;
+  if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46 && charCode !== 43) {
+    $evt.preventDefault();
+  } else {
+    return true;
+  }
+}
+
 function startChat() {
   window.open(`https://wa.me/${phoneNumber.value}`, '_blank')
 }
 </script>
 
 <template>
-  <input
-    type="text"
-    name="wa-me-number"
-    id="waMeNumber"
-    placeholder="Ex: +58412xxxxxxx"
-    class="waMeNumber"
-    v-model="phoneNumber"
-  >
-  <br><br>
-  <button
-    type="button"
-    @click="startChat"
-    :class="{disabledButton: disabledBtn}"
-    :disabled="disabledBtn"
-  >Start chat</button>
+  <p class="copyright"><bold>Developed by Eng. Ernesto Murrugarra</bold></p>
+  <div class="example">
+    <label for="wa-me-number">Write number here:</label>
+    <input
+      type="tel"
+      name="wa-me-number"
+      id="waMeNumber"
+      placeholder="Ex: +58412xxxxxxx"
+      class="waMeNumber"
+      @keypress="NumbersOnly"
+      v-model="phoneNumber"
+    >
+    <br><br>
+    <button
+      type="button"
+      @click="startChat"
+      :class="{disabledButton: disabledBtn}"
+      :disabled="disabledBtn"
+    >Start chat</button>
+  </div>
+  <div class="presentation">
+    <h1 class="title"><i class="fa-brands fa-whatsapp"></i> Whatsapp Me!</h1>
+    <p class="description">Just write a valid number to start</p>
+    <p class="description">a chat on whatsapp.</p>
+    <p class="description">Enjoy!</p>
+  </div>
 </template>
 
-<style scoped>
+<style>
+#app {
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+  width: 100vw;
+}
+
+#app > div {
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+}
+
+.copyright {
+  color: #FFFF;
+  font-size: 1em;
+  padding: 1em;
+  position: absolute;
+}
+
+.example label {
+  color: rgba(61, 180, 76, 1);
+  font-size: 1.5em;
+}
+
+.presentation {
+  background-color: rgba(61, 180, 76, 1);
+  color: #FFFF;
+}
+
+.title {
+  font-size: 3em;
+  padding-bottom: 1em;
+}
+
+.description {
+  font-size: 1.5em;
+}
+
+button {
+  border-radius: 8px;
+  border: 1px solid transparent;
+  color: #000000;
+  padding: 0.6em 1.2em;
+  font-size: 1.3em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: rgba(61, 180, 76, 1);
+  cursor: pointer;
+  transition: border-color 0.25s;
+  transition: 1s;
+}
+button:hover {
+  background-color: rgba(72, 211, 90, 1);
+  border-color: #242424;
+}
+button:focus,
+button:focus-visible {
+  outline: 4px auto -webkit-focus-ring-color;
+}
+
 .waMeNumber {
+  border-radius: 20px;
+  font-size: 1.5em;
+  padding: .5em;
   width: 300px;
-  padding: 10px;
 }
 
 .disabledButton {
@@ -38,9 +125,45 @@ function startChat() {
   color: white;
 }
 
+@media (max-width: 991px) {
+  .copyright {
+    font-size: .9em;
+  }
+  .title {
+    font-size: 2.5em;
+  }
+  .example label, .description {
+    font-size: 1.3em;
+  }
+  button, .waMeNumber {
+    font-size: 1em;
+  }
+}
+
 @media (max-width: 573px) {
-  .waMeNumber {
+  #app {
+    flex-direction: column;
+  }
+  #app > div {
+    height: 50%;
     width: 100%;
+  }
+  .copyright {
+    font-size: .8em;
+    left: 0; 
+    right: 0; 
+    margin-left: auto; 
+    margin-right: auto; 
+    width: fit-content;
+  }
+  .title {
+    font-size: 2em;
+  }
+  .example label, .description {
+    font-size: 1.2em;
+  }
+  button, .waMeNumber {
+    font-size: 1em;
   }
 }
 </style>
