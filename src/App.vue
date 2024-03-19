2c8e2f9e60ed769a4ecf4fb717625bc6e42d1f3b
Ernesto Murrugarra <ernestomurrugarra@Ernestos-MBP.fibertel.com.ar>
2024-03-19 18:38:01 -0300

    Added linked in url

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script setup>
-import { computed, ref } from 'vue'
+import { computed, ref } from "vue";
 
-const phoneNumber = ref('')
-const disabledBtn = computed(() => { return phoneNumber.value === '' })
+const phoneNumber = ref("");
+const disabledBtn = computed(() => {
+  return phoneNumber.value === "";
+});
 
 function NumbersOnly($evt) {
-  $evt = ($evt) ? $evt : window.event;
-  var charCode = ($evt.which) ? $evt.which : $evt.keyCode;
-  if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46 && charCode !== 43) {
+  $evt = $evt ? $evt : window.event;
+  var charCode = $evt.which ? $evt.which : $evt.keyCode;
+  if (
+    charCode > 31 &&
+    (charCode < 48 || charCode > 57) &&
+    charCode !== 46 &&
+    charCode !== 43
+  ) {
     $evt.preventDefault();
   } else {
     return true;
@@ -15,12 +22,23 @@ function NumbersOnly($evt) {
 }
 
 function startChat() {
-  window.open(`https://wa.me/${phoneNumber.value}`, '_blank')
+  window.open(`https://wa.me/${phoneNumber.value}`, "_blank");
 }
 </script>
 
 <template>
-  <p class="copyright"><bold>Developed by Eng. Ernesto Murrugarra</bold></p>
+  <p class="copyright">
+    <bold>
+      Developed by
+      <a
+        href="https://www.linkedin.com/in/murrugarra/"
+        target="_blank"
+        class="link"
+      >
+        Eng. Ernesto Murrugarra
+      </a>
+    </bold>
+  </p>
   <div class="example">
     <label for="wa-me-number">Write number here:</label>
     <input
@@ -31,14 +49,16 @@ function startChat() {
       class="waMeNumber"
       @keypress="NumbersOnly"
       v-model="phoneNumber"
-    >
-    <br><br>
+    />
+    <br /><br />
     <button
       type="button"
       @click="startChat"
-      :class="{disabledButton: disabledBtn}"
+      :class="{ disabledButton: disabledBtn }"
       :disabled="disabledBtn"
-    >Start chat</button>
+    >
+      Start chat
+    </button>
   </div>
   <div class="presentation">
     <h1 class="title"><i class="fa-brands fa-whatsapp"></i> Whatsapp Me!</h1>
@@ -66,10 +86,15 @@ function startChat() {
 }
 
 .copyright {
-  color: #FFFF;
+  color: #ffff;
   font-size: 1em;
   padding: 1em;
   position: absolute;
+}
+
+.link {
+  color: #ffff;
+  text-decoration: none;
 }
 
 .example label {
@@ -79,7 +104,7 @@ function startChat() {
 
 .presentation {
   background-color: rgba(61, 180, 76, 1);
-  color: #FFFF;
+  color: #ffff;
 }
 
 .title {
@@ -116,26 +141,29 @@ button:focus-visible {
 .waMeNumber {
   border-radius: 20px;
   font-size: 1.5em;
-  padding: .5em;
+  padding: 0.5em;
   width: 300px;
 }
 
-.disabledButton, .disabledButton:hover {
+.disabledButton,
+.disabledButton:hover {
   background-color: grey;
   color: white;
 }
 
 @media (max-width: 991px) {
   .copyright {
-    font-size: .9em;
+    font-size: 0.9em;
   }
   .title {
     font-size: 2.5em;
   }
-  .example label, .description {
+  .example label,
+  .description {
     font-size: 1.3em;
   }
-  button, .waMeNumber {
+  button,
+  .waMeNumber {
     font-size: 1em;
   }
 }
@@ -149,20 +177,22 @@ button:focus-visible {
     width: 100%;
   }
   .copyright {
-    font-size: .8em;
-    left: 0; 
-    right: 0; 
-    margin-left: auto; 
-    margin-right: auto; 
+    font-size: 0.8em;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
     width: fit-content;
   }
   .title {
     font-size: 2em;
   }
-  .example label, .description {
+  .example label,
+  .description {
     font-size: 1.2em;
   }
-  button, .waMeNumber {
+  button,
+  .waMeNumber {
     font-size: 1em;
   }
 }
